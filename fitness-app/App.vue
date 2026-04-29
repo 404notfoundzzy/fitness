547a1fad0c0887125dfa02179fa21caee0abced7
zzy <zzy15129802903@126.com>
2026-04-29 15:04:39 +0800

    feat(frontend): 添加前端应用配置文件
    - 添加应用配置 manifest.json
    - 添加页面配置 pages.json
    - 添加应用入口 App.vue
    - 添加主入口文件 main.js

---
+++ fitness-app/App.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/* 每个页面公共css */
+	
+	/* 适配各种自定义样式 */
+	.container {
+		box-sizing: border-box;
+	}
+</style>
